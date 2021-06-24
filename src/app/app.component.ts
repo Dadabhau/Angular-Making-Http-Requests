7cbac7c76c8e0bb38247e36bd7ea8a302e9d0fca
dadabhau <dadabhaut@ithena.ai>
2021-06-24 21:04:39 +0530

    Showing a Loading Indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetchingPosts = false;
 
   constructor(private http: HttpClient) {}
 
@@ -40,6 +41,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetchingPosts = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-d522c-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json'
@@ -58,6 +60,7 @@ export class AppComponent implements OnInit {
       .subscribe((posts) => {
         // ...
         console.log(posts);
+        this.isFetchingPosts = false;
         this.loadedPosts = posts;
       });
   }
