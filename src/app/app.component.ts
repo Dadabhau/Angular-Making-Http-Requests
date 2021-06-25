d182566554c0c8e47da7f137d17ff2fb21b3cc4a
dadabhau <dadabhaut@ithena.ai>
2021-06-25 20:44:12 +0530

    Setting Headers and Adding Query Params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import { Post } from './post.model';
-import { PostService } from './post.service';
+import { PostsService } from './posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[] = [];
-  isFetchingPosts = false;
+  isFetching = false;
   error = null;
+  private errorSub: Subscription;
 
-  constructor(private http: HttpClient, private postService: PostService) {}
+  constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
-    const isFetchingPosts = true;
-    this.postService.fetchPosts().subscribe(
+    this.errorSub = this.postsService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
+
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
       (posts) => {
-        this.isFetchingPosts = false;
+        this.isFetching = false;
         this.loadedPosts = posts;
       },
       (error) => {
+        this.isFetching = false;
         this.error = error.message;
       }
     );
@@ -32,18 +39,19 @@ export class AppComponent implements OnInit {
 
   onCreatePost(postData: Post) {
     // Send Http request
-    this.postService.createAndStorePost(postData.title, postData.content);
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
     // Send Http request
-    this.isFetchingPosts = true;
-    this.postService.fetchPosts().subscribe(
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
       (posts) => {
-        this.isFetchingPosts = false;
+        this.isFetching = false;
         this.loadedPosts = posts;
       },
       (error) => {
+        this.isFetching = false;
         this.error = error.message;
         console.log(error);
       }
@@ -52,8 +60,16 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
-    this.postService.deletePosts().subscribe(() => {
+    this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
